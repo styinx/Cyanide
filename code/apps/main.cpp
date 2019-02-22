9dcfd563b5f7403fdc27e22c74e89144b73748b2
styinx <zornchrisjo@googlemail.com>
2019-02-22 19:24:29 +0100

    initialize cmake and travis

---
+++ code/apps/main.cpp
@@ -0,0 +1,6 @@
+#include "audio/audio_devices/AudioDevice.hpp"
+
+int main()
+{
+    return 0;
+}
---
+++ code/include/audio/interfaces/IAudioDriver.hpp
@@ -0,0 +1,26 @@
+/*
+ * Created by styinx on 21.02.19.
+ * Copyright (c) 2019 styinx All rights reserved.
+ */
+
+#ifndef CYANIDE_IAUDIODRIVER_HPP
+#define CYANIDE_IAUDIODRIVER_HPP
+
+namespace Cyanide
+{
+namespace audio
+{
+
+    class IAudioDriver
+    {
+    private:
+    public:
+        IAudioDriver() = default;
+
+        ~IAudioDriver() = default;
+    };
+
+} // namespace audio
+} // namespace Cyanide
+
+#endif // CYANIDE_IAUDIODRIVER_HPP
