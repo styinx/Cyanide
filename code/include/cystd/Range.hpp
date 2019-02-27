5bf30c28b5c24e023f3c2d4eafca8e19f6ff8151
styinx <zornchrisjo@googlemail.com>
2019-02-27 19:40:21 +0100

    update prototypes; add range class

---
+++ code/include/cystd/Range.hpp
@@ -0,0 +1,101 @@
+#ifndef CYANIDE_RANGE_HPP
+#define CYANIDE_RANGE_HPP
+
+namespace Cyanide
+{
+namespace cystd
+{
+
+    template<typename T>
+    class Range
+    {
+    public:
+        class Iterator
+        {
+        public:
+            T current;
+            T start;
+            T stop;
+            T step;
+
+            Iterator(T current, T start, T stop, T step)
+                : current(current)
+                , start(start)
+                , stop(stop)
+                , step(step)
+            {
+            }
+
+            T operator*() const
+            {
+                return current;
+            }
+
+            explicit operator T&()
+            {
+                return current;
+            }
+
+            Iterator& operator++()
+            {
+                if(start <= stop)
+                {
+                    current += step;
+                }
+                else
+                {
+                    current -= step;
+                }
+                return *this;
+            }
+
+            bool operator==(const T& other) const
+            {
+                if(start <= stop)
+                {
+                    return current >= other;
+                }
+                else if(start > stop)
+                {
+                    return current <= other;
+                }
+            }
+
+            bool operator!=(const T& other) const
+            {
+                return current != other;
+            }
+        };
+
+        T start;
+        T stop;
+        T step;
+
+        Range(T from, T to, T step)
+            : start(from)
+            , stop(to)
+            , step(step)
+        {
+        }
+
+        Iterator begin() const
+        {
+            return Iterator(start, start, stop, step);
+        };
+
+        Iterator end() const
+        {
+            return Iterator(stop, start, stop, step);
+        };
+    };
+
+    template<typename T>
+    Range<T> range(T from, T to, T step)
+    {
+        return Range<T>(from, to, step);
+    }
+
+}  // namespace cystd
+}  // namespace Cyanide
+
+#endif  // CYANIDE_RANGE_HPP
