efe906a627ae58e2e15e045ff1884443d3039b6f
styinx <zornchrisjo@googlemail.com>
2019-03-01 22:41:52 +0100

    add ISocket

---
+++ code/include/cynet/ISocket.hpp
@@ -0,0 +1,21 @@
+#ifndef CYANIDE_ISOCKET_HPP
+#define CYANIDE_ISOCKET_HPP
+
+namespace Cyanide
+{
+namespace cynet
+{
+
+    class ISocket
+    {
+    private:
+    public:
+        ISocket() = default;
+
+        ~ISocket() = default;
+    };
+
+}  // namespace cynet
+}  // namespace Cyanide
+
+#endif  // CYANIDE_ISOCKET_HPP
