e840e9857d31345d5f80e4e82cb4ff37ccd24be3
styinx <zornchrisjo@googlemail.com>
2019-12-07 19:22:26 +0100

    Add pip requirements

--- scripts/install.sh
+++ scripts/install.sh
@@ -8,6 +8,14 @@ sudo apt-get install -y libsdl2-mixer-2.0.0 libsdl2-mixer-dev
 sudo apt-get install -y libsdl2-net-2.0.0 libsdl2-net-dev
 sudo apt-get install -y libsdl2-ttf-2.0.0 libsdl2-ttf-dev
 
-python3 -m pip install conan
-mkdir -p build
-sudo conan install . -if build
+python3 -m pip install virtualenv
+python3 -m virtualenv venv/ -p python3
+. ./venv/bin/activate
+
+python3 -m pip install -r requirements.txt
+
+mkdir build
+conan install . -if build
+
+deactivate ./venv/bin/
+
