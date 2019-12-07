e9e9efbccb2039523f76491ca17c6dd2671ffe2b
styinx <zornchrisjo@googlemail.com>
2019-12-07 17:02:27 +0100

    Changes version of sdlgfx and call to conan

--- script/install.sh
+++ script/install.sh
@@ -2,12 +2,12 @@
 
 sudo apt-get update
 sudo apt-get install -y libsdl2-2.0.0 libsdl2-dev
-sudo apt-get install -y libsdl2-gfx-1.0.4 libsdl2-gfx-dev
+sudo apt-get install -y libsdl2-gfx-1.0.0 libsdl2-gfx-dev
 sudo apt-get install -y libsdl2-image-2.0.0 libsdl2-image-dev
 sudo apt-get install -y libsdl2-mixer-2.0.0 libsdl2-mixer-dev
 sudo apt-get install -y libsdl2-net-2.0.0 libsdl2-net-dev
 sudo apt-get install -y libsdl2-ttf-2.0.0 libsdl2-ttf-dev
 
 python3 -m pip install conan
-mkdir build
-python3 -m conan install . -if build
+mkdir -p build
+sudo conan install . -if build
