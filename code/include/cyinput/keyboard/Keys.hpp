87302b2f39f7a3193475ea300a440c8e8dc04633
styinx <zornchrisjo@googlemail.com>
2019-12-20 14:49:30 +0100

    Add mouse button and motion to event loop

--- code/include/cyinput/keyboard/Keys.hpp
+++ code/include/cyinput/keyboard/Keys.hpp
@@ -9,7 +9,17 @@ namespace cyanide::cyinput
     using KeyCode = Uint16;
     using KeyMod  = Uint16;
 
+    using MouseButtonCode = Uint8;
+
     enum class KEY_STATE : Byte
+    {
+        NONE     = 0x00,
+        RELEASED = 0x01,
+        DOWN     = 0x02,
+        PRESSED  = 0x04
+    };
+
+    enum class MOUSE_BUTTON_STATE : Byte
     {
         NONE     = 0x00,
         RELEASED = 0x01,
